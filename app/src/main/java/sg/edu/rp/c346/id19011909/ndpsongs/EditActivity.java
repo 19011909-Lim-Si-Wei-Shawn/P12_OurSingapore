df6eb43420ff388f9631d5b5a4a94d731042af2a
Shawn <19011909@myrp.edu.sg>
2021-08-11 11:59:14 +0800

    Updated~

--- app/src/main/java/sg/edu/rp/c346/id19011909/ndpsongs/EditActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id19011909/ndpsongs/EditActivity.java
@@ -1,7 +1,9 @@
 package sg.edu.rp.c346.id19011909.ndpsongs;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -77,11 +79,36 @@ public class EditActivity extends AppCompatActivity {
         BtnDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DBHelper dbh = new DBHelper(EditActivity.this);
-                dbh.deleteSong(data.get_id());
 
-                Intent i = new Intent(EditActivity.this, DisplayActivity.class);
-                startActivity(i);
+                //Triggering Dialog,
+                AlertDialog.Builder myBuilder = new AlertDialog.Builder(EditActivity.this);
+                myBuilder.setTitle("Danger");
+                myBuilder.setMessage("Are You Sure you want to Delete the Island\n" + ETSongEdit.getText().toString());
+                myBuilder.setCancelable(false);
+
+                //Configure the 'Positive' Button,
+                myBuilder.setPositiveButton("CANCEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                //Configure the 'Negative' Button,
+                myBuilder.setNegativeButton("DELETE", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        DBHelper dbh = new DBHelper(EditActivity.this);
+                        dbh.deleteSong(data.get_id());
+
+                        Intent i = new Intent(EditActivity.this, DisplayActivity.class);
+                        startActivity(i);
+                    }
+                });
+
+                AlertDialog myDialog = myBuilder.create();
+                myDialog.show();
 
             }
         });
@@ -89,8 +116,33 @@ public class EditActivity extends AppCompatActivity {
         BtnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(EditActivity.this, DisplayActivity.class);
-                startActivity(i);
+
+                //Triggering Dialog,
+                AlertDialog.Builder myBuilder = new AlertDialog.Builder(EditActivity.this);
+                myBuilder.setTitle("Danger");
+                myBuilder.setMessage("Are You Sure you want to Discard the Changes");
+                myBuilder.setCancelable(false);
+
+                //Configure the 'Positive' Button,
+                myBuilder.setPositiveButton("DO NOT DISCARD", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                //Configure the 'Negative' Button,
+                myBuilder.setNegativeButton("DISCARD", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        Intent i = new Intent(EditActivity.this, DisplayActivity.class);
+                        startActivity(i);
+                    }
+                });
+
+                AlertDialog myDialog = myBuilder.create();
+                myDialog.show();
             }
         });
 
